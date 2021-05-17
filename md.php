b7eabe6e6cd75d95849c5be1659bde04c4a57dfc
Zack Garza <dzackgarza@gmail.com>
2021-05-16 20:30:54 -0400

    / ‘md.php’

--- md.php
+++ md.php
@@ -4,11 +4,15 @@
 
 	$css         = "/md.css";
 
-	$md_fileroot = "/media/dropbox/Notes";
-	$md_webroot  = "/notes";
+	$md_fileroot = "/var/www/Notes";
+	$md_webroot  = "/var/www/Notes";
 	$md_filepath = str_replace($md_webroot,$md_fileroot,$infile);	
 	$md_filepath = '"' . urldecode($md_filepath) . '"';
 	$command     = "$pandoc -s -c -f markdown_github+yaml_metadata_block $md_filepath -t html";
+
+  $myfile = fopen("/var/www/log.txt", "w") or die("Unable to open file!");
+  fwrite($myfile, $infile);
+  fclose($myfile);
 
 	echo shell_exec($command);
 ?>
